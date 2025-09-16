100818739a3bb742525f8ac3cf14741c9fc60269
Vanessa <vanessakausl@gmail.com>
2025-09-16 20:44:30 +0200

    formatting

--- rest/src/main/java/at/fhtw/rest/controller/StatusController.java
+++ rest/src/main/java/at/fhtw/rest/controller/StatusController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class StatusController implements StatusApi {
+
     @Override
     public ResponseEntity<String> getStatus() {
         return ResponseEntity.ok("Alive.");
