9dccf8a00bf4badc20728776a55d4cc3acda54f3
Ron <if22b080@technikum-wien.at>
2025-09-17 00:47:33 +0200

    added logger

--- rest/src/main/java/at/fhtw/rest/persistence/repository/DocumentRepositoryImpl.java
+++ rest/src/main/java/at/fhtw/rest/persistence/repository/DocumentRepositoryImpl.java
@@ -2,11 +2,13 @@ package at.fhtw.rest.persistence.repository;
 
 import at.fhtw.rest.persistence.entity.DocumentEntity;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Repository
 @AllArgsConstructor
 public class DocumentRepositoryImpl implements DocumentRepository {
@@ -14,21 +16,25 @@ public class DocumentRepositoryImpl implements DocumentRepository {
 
     @Override
     public DocumentEntity save(DocumentEntity document) {
+        log.trace("Repo.save({})", document);
         return repository.save(document);
     }
 
     @Override
     public List<DocumentEntity> findAll() {
+        log.trace("Repo.findAll()");
         return repository.findAll();
     }
 
     @Override
     public Optional<DocumentEntity> findById(Long id) {
+        log.trace("Repo.findById({})", id);
         return repository.findById(id);
     }
 
     @Override
     public void deleteAll() {
+        log.warn("Repo.deleteAll()");
         repository.deleteAll();
     }
 }
