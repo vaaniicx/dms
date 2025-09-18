11fc7e2ba2d4981edb0efe53d2efc497160388dc
l3x61 <l3x61@pm.me>
2025-09-18 17:17:13 +0200

    unabstract repository

--- rest/src/main/java/at/fhtw/rest/persistence/repository/DocumentRepository.java
+++ rest/src/main/java/at/fhtw/rest/persistence/repository/DocumentRepository.java
@@ -1,16 +1,9 @@
 package at.fhtw.rest.persistence.repository;
 
 import at.fhtw.rest.persistence.entity.DocumentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface DocumentRepository {
-    DocumentEntity save(DocumentEntity document);
-
-    List<DocumentEntity> findAll();
-
-    Optional<DocumentEntity> findById(Long id);
-
-    void deleteAll();
-}
+public interface DocumentRepository extends JpaRepository<DocumentEntity, Long> {}
