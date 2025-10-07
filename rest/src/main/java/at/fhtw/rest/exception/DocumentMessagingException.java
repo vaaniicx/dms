afd59b4585cff2428426bee9ac192a08675dd655
l3x61 <l3x61@pm.me>
2025-10-07 21:49:32 +0200

    rabbitMQ communication between OCR and REST

--- rest/src/main/java/at/fhtw/rest/exception/DocumentMessagingException.java
+++ rest/src/main/java/at/fhtw/rest/exception/DocumentMessagingException.java
@@ -1,10 +1,6 @@
 package at.fhtw.rest.exception;
 
-/**
- * Signals failures while interacting with the messaging infrastructure.
- */
 public class DocumentMessagingException extends RuntimeException {
-
     public DocumentMessagingException(String message, Throwable cause) {
         super(message, cause);
     }
