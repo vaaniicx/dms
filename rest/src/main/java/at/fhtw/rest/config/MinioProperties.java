2dc9e7f467a7db820036ee47b412be516251af27
alex <l3x61@pm.me>
2025-10-25 17:49:40 +0200

    Refactor MinioProperties to remove validation annotations

--- rest/src/main/java/at/fhtw/rest/config/MinioProperties.java
+++ rest/src/main/java/at/fhtw/rest/config/MinioProperties.java
@@ -1,22 +1,11 @@
 package at.fhtw.rest.config;
 
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
 
-@Validated
 @ConfigurationProperties(prefix = "minio")
 public record MinioProperties(
-        @NotBlank
-        String endpoint,
-
-        @NotBlank
-        String accessKey,
-
-        @NotBlank
-        String secretKey,
-
-        @NotBlank
-        String bucket
-) {
-}
+    String endpoint,
+    String accessKey,
+    String secretKey,
+    String bucket
+) {}
