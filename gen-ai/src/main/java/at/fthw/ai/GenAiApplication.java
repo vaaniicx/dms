b1a4682a8d77e1ff9f95f81f9ad5cebf20715168
Vanessa <vanessakausl@gmail.com>
2025-11-14 22:03:09 +0100

    add gen-ai module

---
+++ gen-ai/src/main/java/at/fthw/ai/GenAiApplication.java
@@ -0,0 +1,12 @@
+package at.fthw.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenAiApplication.class, args);
+    }
+}
