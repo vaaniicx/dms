2deb26c49e4fc23c1dd9d0fe1b481c2ddde6d6a1
Ron <if22b080@technikum-wien.at>
2026-01-02 20:50:02 +0100

    added queue for document statistics and batch process

--- dms-commons/src/main/java/at/fhtw/message/QueueName.java
+++ dms-commons/src/main/java/at/fhtw/message/QueueName.java
@@ -8,6 +8,8 @@ public class QueueName {
 
     public static final String REST_DOCUMENT_SUMMARIZED = "rest.document.summarized.queue";
 
+    public static final String REST_DOCUMENT_STATISTICS = "rest.document.statistics.queue";
+
     public static final String OCR_DOCUMENT_UPLOADED = "ocr.document.uploaded.queue";
 
     public static final String GENAI_DOCUMENT_SCANNED = "genai.document.scanned.queue";
